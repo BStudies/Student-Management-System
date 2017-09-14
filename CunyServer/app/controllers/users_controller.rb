04ce7c01a9c0e271abd3fd3d86e813d2ad8f9e08
Brandon Hew <digihew@gmail.com>
2017-09-14 10:06:38 -0400

    update users controller

--- CunyServer/app/controllers/users_controller.rb
+++ CunyServer/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
-class UsersController < ApplicationController
+class UsersController < ApiController
   before_action :set_user, only: [:show, :update, :destroy]
-
+  before_action :require_login, except: [:create]
   # GET /users
   def index
     @users = User.all
@@ -9,16 +9,27 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1
-  def show
-    render json: @user
+  # def show
+  #   render json: @user
+  # end
+  def profile
+    user = User.find_by_auth_token!(request.headers[:token])
+    render json: {
+      user: {
+        username: user.username, 
+        email: user.email, 
+        name: user.name}
+      }
   end
+  
 
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      # render json: @user, status: :created, location: @user
+      render json: {token: user.auth_token}
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +57,7 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.fetch(:user, {})
+      # params.fetch(:user, {})
+      params.require(:user).permit(:username, :email, :password, :name)
     end
 end
