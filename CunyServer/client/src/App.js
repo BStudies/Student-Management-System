dee185021bcdc715fd61329897e21e5bbaa07850
brandon <digihew@gmail.com>
2017-09-14 18:24:50 -0400

    Start routes in client

--- CunyServer/client/src/App.js
+++ CunyServer/client/src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './modules/Auth'
+import Auth from './modules/Auth'
 
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      auth: isUserAuthenticated(),
+      auth: Auth.isUserAuthenticated(),
       shouldRedirect: false,
     }
   }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className="App">
+          <Route exact path="/" render={ () => <Home/>}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
+      
     );
   }
 }
