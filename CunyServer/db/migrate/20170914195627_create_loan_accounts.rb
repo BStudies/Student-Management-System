56982b8c8fb301acaa0580ec29f9b90fa37d7efa
Brandon Hew <digihew@gmail.com>
2017-09-14 15:56:48 -0400

    Create Loan account

---
+++ CunyServer/db/migrate/20170914195627_create_loan_accounts.rb
@@ -0,0 +1,9 @@
+class CreateLoanAccounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :loan_accounts do |t|
+      t.belongs_to :financial_account
+
+      t.timestamps
+    end
+  end
+end
