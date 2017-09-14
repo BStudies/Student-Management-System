f503e9f1df3aab846c32b30e1178569fb38c248b
Brandon Hew <digihew@gmail.com>
2017-09-14 09:55:25 -0400

    setup auth routes

--- CunyServer/config/routes.rb
+++ CunyServer/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+  # treat user as an abstract class
+  resources :users, only: [:index, :new, :show]
+  
+  # authentication routes
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/profile", to: "users#profile"
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
