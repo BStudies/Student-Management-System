ba86480bc73f2dc0b00fb338b76b90bc0b8a82fb
brandon <digihew@gmail.com>
2017-09-14 18:15:05 -0400

    setup react app and auth.js

---
+++ CunyServer/client/src/modules/Auth.js
@@ -0,0 +1,22 @@
+class Auth{
+    // https://vladimirponomarev.com/blog/authentication-in-react-apps-jwt
+    static authenticateToken(token){
+        sessionStorage.setItem('token', token);
+    }
+
+    static isUserAuthenticated(){
+        return sessionStorage.getItem('token') !== null;
+    }
+
+
+    static deauthenticateUser(){
+        return sessionStorage.removeItem('token');
+    }
+
+    static getToken(){
+        return sessionStorage.getItem('token');
+    }
+}
+
+
+export default Auth;
