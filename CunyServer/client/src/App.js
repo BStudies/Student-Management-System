3057a3389f6964858c2b45dbdc8a797f24067001
brandon <digihew@gmail.com>
2017-09-14 18:30:05 -0400

    change components from js to jsx and start routing

--- CunyServer/client/src/App.js
+++ CunyServer/client/src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Auth from './modules/Auth'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { Redirect } from 'react-router';
 
+
+
+// components
+import Home from './components/Home'
 
 class App extends Component {
   constructor(){
@@ -17,6 +23,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Route exact path="/" render={ () => <Home/>}/>
+          {/* {this.redirectTo()} */}
         </div>
       </Router>
       
