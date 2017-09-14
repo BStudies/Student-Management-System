4ec9f70da7cd3b01a8c86a0390f45d6866840674
Brandon Hew <digihew@gmail.com>
2017-09-14 09:28:01 -0400

    Create api and session controller

--- CunyServer/db/migrate/20170914132219_create_users.rb
+++ CunyServer/db/migrate/20170914132219_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :username, index: true
+      t.string :password_digest
+      t.string :email
+      t.string :name
+      t.string :auth_token
 
       t.timestamps
     end
