9e90476649da9715aef22441479007384e14bcca
Brandon Hew <digihew@gmail.com>
2017-09-14 09:34:12 -0400

    define authenticate_token in api controller

--- CunyServer/app/controllers/api_controller.rb
+++ CunyServer/app/controllers/api_controller.rb
@@ -1,7 +1,26 @@
 class ApiController < ApplicationController
+    # checks if current requester is logged in
     def require_login
         authenticate_token ||= render_unauthorized("Access denied")
     end
 
+    def current_user
+        @current_user ||= authenticate_token
+    end
+    
+    protected
+    # if request is unauthorized, deny
+    def render_unauthorized(message)
+        errors = {errors: [detail: message]}
+        render json: errors, status: :unauthorized
+    end
+
+    private
+    def authenticate_token
+        authenticate_with_http_token do |token, options|
+            User.find_by(auth_token: token)
+        end
+    end
+    
     
 end
--- CunyServer/app/controllers/sessions_controller.rb
+++ CunyServer/app/controllers/sessions_controller.rb
@@ -1,2 +1,2 @@
-class SessionsController < ApplicationController
+class SessionsController < ApiController
 end
