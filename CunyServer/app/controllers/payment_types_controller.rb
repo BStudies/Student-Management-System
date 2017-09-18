7bccbfcb3b0c7bcbf03c9d574c1beb0c15ca8f7f
Brandon Hew <digihew@gmail.com>
2017-09-18 11:52:47 -0400

    remove value from financial account, its supposed to be in each payment type

--- CunyServer/app/controllers/payment_types_controller.rb
+++ CunyServer/app/controllers/payment_types_controller.rb
@@ -1,2 +1,38 @@
 class PaymentTypesController < ApplicationController
+    def create
+        payment_type = PaymentType.new()
+        payment_type.student = current_user.person.student
+        payment_type.save!
+
+        #each need to be linked with respective accounts
+        f=Fafsa.new({value: 0})
+        f.payment_type = payment_type
+        f.save!
+
+        t=Tap.new({value: 0})
+        t.payment_type = payment_type
+        t.save!
+
+        l=Loan.new({value: 0})
+        l.payment_type = payment_type
+        l.save!
+
+        s=Scholarship.new({value: 0})
+        s.payment_type = payment_type
+        s.save!
+
+        o=OutOfPocket.new({value: 0});
+        o.payment_type = payment_type
+        o.save!
+
+        payment_type.fafsa = 
+        payment_type.tap = 
+        payment_type.loan = 
+        payment_type.scholarship = 
+        payment_type.out_of_pocket = 
+        
+        fafsa.payment_type = payment_type
+        payment_type.fafsa = 
+    end 
+    
 end
--- CunyServer/db/migrate/20170914195247_create_financial_accounts.rb
+++ CunyServer/db/migrate/20170914195247_create_financial_accounts.rb
@@ -1,7 +1,6 @@
 class CreateFinancialAccounts < ActiveRecord::Migration[5.1]
   def change
     create_table :financial_accounts do |t|
-      t.integer :value
       t.string :name
       t.timestamps
     end
